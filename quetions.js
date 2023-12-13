3b25269dfed8272c651a92ec7e47627b3ec08d76
EL HOSAYN IBOURK <78323417+Ibourk-El@users.noreply.github.com>
2023-12-13 22:15:30 +0100

    add commit

---
+++ quetions.js
@@ -0,0 +1,160 @@
+export let quetions = {
+  football: [
+    {
+      gif: "./img/football.gif",
+      img: "./img/football.png",
+      bg:"./img/bg2.jpg",
+      sound:"Ronaldo-suii-1.mp3"
+    },
+    {
+      question: "Who won the 2022 FIFA World Cup?",
+      answer: "France",
+    },
+    {
+      question:
+        "Which player holds the record for the most goals in a single Premier League season?",
+      answer: "Shearer",
+    },
+    {
+      question: "Which country hosted the 2018 FIFA World Cup?",
+      answer: "Russia",
+    },
+    {
+      question:
+        "Who is the all-time leading goal scorer in the UEFA Champions League?",
+      answer: "Ronaldo",
+    },
+    {
+      question: "Which team has won the most UEFA Champions League titles?",
+      answer: "Real Madrid",
+    },
+    {
+      question:
+        "Who is the current captain of the Argentina national football team?",
+      answer: "Messi",
+    },
+    {
+      question: "Which club does Mohamed Salah play for?",
+      answer: "Liverpool",
+    },
+    {
+      question: "Who is the manager of Manchester City as of 2023?",
+      answer: "Guardiola",
+    },
+    {
+      question: "Which stadium is known as the 'Theatre of Dreams'?",
+      answer: "Old Trafford",
+    },
+    {
+      question: "Who won the Ballon d'Or in 2021?",
+      answer: "Messi",
+    },
+  ],
+  tennis: [
+    {
+      gif: "./img/tennis.gif",
+      img: "./img/tennis.png",
+      bg:"./img/bg2.jpg",
+      sound:"Ronaldo-suii-1.mp3"
+    },
+    {
+      question:
+        "Who is the all-time Grand Slam singles titles leader in men's tennis?",
+      answer: "Federer",
+    },
+    {
+      question: "Which player is known as the 'King of Clay'?",
+      answer: "Nadal",
+    },
+    {
+      question:
+        "Who holds the record for the most consecutive weeks as the world No. 1 in women's tennis?",
+      answer: "Osaka",
+    },
+    {
+      question:
+        "Which tournament is played on grass courts and is the oldest tennis tournament in the world?",
+      answer: "Wimbledon",
+    },
+    {
+      question:
+        "Who is the most recent winner of the Australian Open in men's singles?",
+      answer: "Djokovic",
+    },
+    {
+      question:
+        "In women's tennis, who has won the most Grand Slam singles titles?",
+      answer: "Court",
+    },
+    {
+      question: "Which country is Novak Djokovic from?",
+      answer: "Serbia",
+    },
+    {
+      question:
+        "Who is known for his powerful serve and is nicknamed 'Iceman'?",
+      answer: "Raonic",
+    },
+    {
+      question:
+        "Which Grand Slam tournament is played on hard courts in New York?",
+      answer: "US Open",
+    },
+    {
+      question: "Who is the Swiss player with 20 Grand Slam singles titles?",
+      answer: "Federer",
+    },
+  ],
+  marathon: [
+    {
+      gif: "./img/marathonrun.gif",
+      img: "./img/marathon.jpg",
+      bg:"./img/bg2.jpg",
+      sound:"Ronaldo-suii-1.mp3"
+    },
+    {
+      question: "In which city is the Boston Marathon held annually?",
+      answer: "Boston",
+    },
+    {
+      question: "What is the standard marathon distance in kilometers?",
+      answer: "42.195",
+    },
+    {
+      question: "Who won the men's marathon at the 2021 Tokyo Olympics?",
+      answer: "Kipchoge",
+    },
+    {
+      question:
+        "What is the name of the famous marathon held in New York City?",
+      answer: "NYC Marathon",
+    },
+    {
+      question:
+        "Which African country is known for producing many elite marathon runners?",
+      answer: "Kenya",
+    },
+    {
+      question: "Who holds the men's world record for the marathon?",
+      answer: "Kipchoge",
+    },
+    {
+      question:
+        "What is the name of the annual midnight marathon held in Norway?",
+      answer: "Midnight Sun Marathon",
+    },
+    {
+      question: "Which river does the Paris Marathon cross during the race?",
+      answer: "Seine",
+    },
+    {
+      question: "Who won the women's marathon at the 2020 Tokyo Olympics?",
+      answer: "Kosgei",
+    },
+    {
+      question: "In which month is the Berlin Marathon usually held?",
+      answer: "September",
+    },
+  ],
+};
+
---
+++ script.js
@@ -0,0 +1,211 @@
+import { quetions } from "./quetions.js";
+let answerBox = document.getElementById("answer-box");
+let chanceEl = document.getElementById("chance");
+let quetionBox = document.getElementById("quetion");
+let menu = document.getElementById("menu");
+let imgChar = document.getElementById("img-char");
+let startBtn = document.querySelector(".start");
+let restartBtn = document.querySelector(".restart");
+let menuBtn = document.querySelector(".menu-btn");
+let nextBtn = document.querySelector(".next");
+let btnsBox = document.querySelector(".btns");
+let cards = document.querySelectorAll(".card");
+let btnsMenu = document.querySelector(".btns");
+let keyboardBox = document.getElementById("keyboard-box");
+let score = document.querySelector(".score");
+
+//
+let scoreNum = 0;
+let catigory = "";
+let catigoryQuetion = quetions[catigory];
+let questionIndex = 1;
+let letters = "azertyuiopmlkjhgfdsqwxcvbn ";
+let chance = 10;
+let heightOfCloser = 100;
+let porsontge = 0;
+//
+// to chose the catigory
+cards.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+    resetCard();
+    catigory = e.target.getAttribute("data-catigory");
+    cards[i].classList.add("active");
+    imgChar.src = quetions[catigory][0].img;
+    document.body.style.backgroundImage = `url(${quetions[catigory][0].bg})`;
+    heightOfCloser = 100;
+    chance = 10;
+    resetVars();
+  });
+});
+// start geme
+startBtn.addEventListener("click", (e) => {
+  if (catigory !== "") menu.style.height = "0%";
+  questionIndex = 1;
+  heightOfCloser = 100;
+  resetVars();
+});
+
+// restart game
+restartBtn.addEventListener("click", (e) => {
+  heightOfCloser = 100;
+  resetVars();
+  btnsBox.style.height = "0%";
+});
+// next game
+nextBtn.addEventListener("click", (e) => {
+  questionIndex++;
+  scoreNum++;
+  heightOfCloser = 100;
+  score.innerHTML = scoreNum;
+  btnsBox.style.height = "0%";
+  imgChar.src = quetions[catigory][0].img;
+  resetVars();
+});
+
+// return to menu
+menuBtn.addEventListener("click", (e) => {
+  menu.style.height = "100%";
+  e.target.parentElement.style.height = "0%";
+  resetCard();
+});
+// reset card style
+function resetCard() {
+  cards.forEach((e) => {
+    e.className = "card";
+  });
+}
+// add tiris to answer Box
+function addTiris(word) {
+  let text = "";
+  for (let i = 0; i < word.length; i++) {
+    text += "-";
+  }
+  return text;
+}
+
+// letters box
+function createLetterBox(value) {
+  let span = document.createElement("span");
+  span.className = "letter";
+  span.innerHTML = value;
+  addEventToLetter(span);
+  span.classList.add("clicked");
+  return span;
+}
+// add chars to answer box
+function addCharToAnswerBox(value) {
+  let span = document.createElement("span");
+  span.className = "answer-letter";
+  span.innerHTML = value;
+  return span;
+}
+// for keyboard
+function pushLettersToBox(className, letters, elFun) {
+  let Box = document.getElementById(className);
+  let len = letters.length;
+  for (let i = 0; i < len; i++) {
+    Box.appendChild(elFun(letters[i]));
+  }
+}
+//add enents keyBord
+function addEventToLetter(letterBox) {
+  letterBox.addEventListener("click", (e) => {
+    if (letterBox.classList.contains("clicked")) {
+      if (
+        checkIfLetterInAnsewr(
+          catigoryQuetion[questionIndex].answer,
+          e.target.innerHTML
+        )
+      ) {
+        e.target.classList.add("true");
+        toTheNextQuetion(heightOfCloser);
+      } else {
+        e.target.classList.add("false");
+        chance--;
+        chanceEl.innerHTML = chance;
+        lose(chance);
+      }
+      letterBox.classList.remove("clicked");
+      playSound("./sounds/mech-keyboard.mp3");
+    }
+  });
+}
+//
+function checkIfLetterInAnsewr(answer, char) {
+  let len = answer.length;
+  let isFinedIt = false;
+  for (let i = 0; i < len; i++) {
+    if (answer[i].toLowerCase() === char) {
+      answerBox.children[i].innerHTML = answer[i];
+      isFinedIt = true;
+      heightOfCloser -= porsontge;
+      console.log(heightOfCloser, porsontge, "x");
+      removeThCloser(heightOfCloser);
+    }
+  }
+  return isFinedIt;
+}
+// show btns menu win lose
+function lose(x) {
+  if (x == 0) {
+    btnsMenu.style.height = "100%";
+    nextBtn.style.display = "none";
+
+    playSound("./sounds/error.wav");
+    removeThCloser(catigoryQuetion);
+  } else {
+    console.log(x);
+  }
+}
+
+// to show a part of the image
+function removeThCloser(pos) {
+  let closeBox = document.getElementById("close");
+  closeBox.style.height = pos + "%";
+}
+// next quetion
+function toTheNextQuetion(hOfC) {
+  if (hOfC <= 0) {
+    if (questionIndex < catigoryQuetion.length - 1) {
+      // win logic
+      btnsMenu.style.height = "100%";
+      nextBtn.style.display = "inline-block";
+      imgChar.src = quetions[catigory][0].gif;
+      heightOfCloser = 0;
+      playSound("./sounds/win.mp3");
+      resetVars();
+    } else {
+      btnsMenu.style.height = "100%";
+      nextBtn.style.display = "none";
+      removeThCloser(0);
+      questionIndex = 1;
+    }
+  } else {
+    console.log(hOfC);
+  }
+}
+// to reset data
+function resetVars() {
+  catigoryQuetion = quetions[catigory];
+  quetionBox.innerHTML = catigoryQuetion[questionIndex].question;
+  chance = 10;
+  porsontge = 100 / catigoryQuetion[questionIndex].answer.length;
+  porsontge += 0.01;
+  removeThCloser(heightOfCloser);
+
+  // reset answer Box
+  answerBox.innerHTML = "";
+  pushLettersToBox(
+    "answer-box",
+    addTiris(catigoryQuetion[questionIndex].answer),
+    addCharToAnswerBox
+  );
+  // reset keyboard
+  keyboardBox.innerHTML = "";
+  pushLettersToBox("keyboard-box", letters, createLetterBox);
+}
+
+function playSound(audioName) {
+  let audio = new Audio(audioName);
+  audio.play();
+}
